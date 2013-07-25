730c6f03688887d2306ef5b1ea70abc62bfefec0
Kevin Kor <kevinkor@rocketmail.com>
2013-07-25 08:57:58 -0700

    testingPHP.php

---
+++ testingPHP.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <link type="text/css" rel="stylesheet" href="style.css"/>
+		<title>First PHP file</title>
+	</head>
+	<body>
+		<h1> Ontraport! </h1>
+        <p>
+           <?php
+			echo "Hello Pin...";
+			echo "How was San Diego?"
+			?> 
+        </p>   
+	</body>
+</html>
+
